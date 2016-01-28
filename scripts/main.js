99457cc9fe264d930eacf61ffd993a30e3cdf0d0
carlosortizv <c.ortiz.vizcarra@accenture.com>
2016-01-28 13:42:52 -0600

    Adding input messages and form styling

--- scripts/main.js
+++ scripts/main.js
@@ -8,10 +8,60 @@ $(function () {
         navigation: false,
         slideSpeed: 300,
         paginationSpeed: 400,
-        singleItem: true,
+        singleItem: true
     });
 
-    $(".form-inline").validate();
+    $(".form-inline").validate({
+    rules: {
+        name: {
+          required: true
+        },
+        lastName: {
+          required: true
+        },
+        email: {
+          required: true
+        },
+        phone: {
+          required: true
+        },
+        state: {
+          required: true
+        },
+        city: {
+          required: true
+        }       
+        
+      },
+      success: function() {
+          //$('.success').show();
+      },
+      highlight: function() {
+          
+      },
+      messages: {
+        name: {
+            required:"Campo requerido"  
+        },
+        lastName: {
+            required:"Campo requerido"
+        },
+        email: {
+          required: "Campo requerido",
+          email: "Este correo es inválido."
+        },
+        phone: {
+          required: "Campo requerido"
+        },
+        state: {
+          required: "Favor de elegir una opción."
+        },
+        city: {
+          required: "Campo requerido"
+        }
+        
+      }
+    });
 
 
 });
